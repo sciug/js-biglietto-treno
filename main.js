3725ad10bf412e02c919fd136ba142b6094be24c
sciug <gaetano.sciurti@libero.it>
2021-10-11 14:00:33 +0200

    add distance, userAge, price variables

---
+++ main.js
@@ -0,0 +1,11 @@
+const distance = parseInt(prompt("quanti chilometri vuoi percorrere?"));
+const userAge = parseInt(prompt("quanti anni hai?"));
+const price = distance * 0.21
+
+
+ if (userAge < 18){
+     var discount = price*20/100;
+     console.log("discount equals:" + discount);
+     var finalPrice = price - discount;
+     console.log("Final price equals: " + finalPrice.toFixed(2));
+} 
