eefca34874b63880ab87e934f2fa49b14e6939a3
sciug <gaetano.sciurti@libero.it>
2021-10-11 15:01:57 +0200

    add style

--- main.js
+++ main.js
@@ -8,20 +8,20 @@ document.getElementById("km").textContent = distance
  if (userAge < 18){
      var discount = price*20/100;
      console.log("discount equals:" + discount);
-     document.getElementById("discount").textContent = discount
+     document.getElementById("discount").textContent = discount.toFixed(2) + "$"
      var finalPrice = price - discount;
      console.log("Final price equals: " + finalPrice.toFixed(2));
-     document.getElementById("price").textContent = finalPrice.toFixed(2)
+     document.getElementById("price").textContent = finalPrice.toFixed(2) + "$"
 } else if (userAge > 65){
     var discount = price*40/100;
     console.log("discount equals:" + discount);
-    document.getElementById("discount").textContent = discount
+    document.getElementById("discount").textContent = discount.toFixed(2) + "$"
     var finalPrice = price - discount;
     console.log("Final price equals: " + finalPrice.toFixed(2));
-    document.getElementById("price").textContent = finalPrice.toFixed(2)
+    document.getElementById("price").textContent = finalPrice.toFixed(2) + "$"
 
     
 }else{
     console.log(price)
-     document.getElementById("price").textContent = price.toFixed(2)
+     document.getElementById("price").textContent = price.toFixed(2) + "$"
 }
