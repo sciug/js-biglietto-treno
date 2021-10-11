cd07ff45b1b9673fb3c92e63ff28af907a757da8
sciug <gaetano.sciurti@libero.it>
2021-10-11 15:36:37 +0200

    add bonus

--- main.js
+++ main.js
@@ -1,27 +1,26 @@
 const distance = parseInt(prompt("quanti chilometri vuoi percorrere?"));
 const userAge = parseInt(prompt("quanti anni hai?"));
+const userName = prompt("bonus: inserisci il tuo nome")
+const userEmail = prompt("bonus: inserisci la tua email")
 const price = distance * 0.21;
 document.getElementById("age").textContent = userAge;
-document.getElementById("km").textContent = distance
+document.getElementById("km").textContent = distance;
+document.getElementById("name").textContent = userName;
+document.getElementById("email").textContent = userEmail;
 
 
  if (userAge < 18){
      var discount = price*20/100;
-     console.log("discount equals:" + discount);
      document.getElementById("discount").textContent = discount.toFixed(2) + "$"
      var finalPrice = price - discount;
-     console.log("Final price equals: " + finalPrice.toFixed(2));
      document.getElementById("price").textContent = finalPrice.toFixed(2) + "$"
 } else if (userAge > 65){
     var discount = price*40/100;
-    console.log("discount equals:" + discount);
     document.getElementById("discount").textContent = discount.toFixed(2) + "$"
     var finalPrice = price - discount;
-    console.log("Final price equals: " + finalPrice.toFixed(2));
     document.getElementById("price").textContent = finalPrice.toFixed(2) + "$"
 
     
 }else{
-    console.log(price)
      document.getElementById("price").textContent = price.toFixed(2) + "$"
 }
