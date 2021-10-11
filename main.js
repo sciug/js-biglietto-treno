5d525d616db5fa6a0aefa1c1424c88fc7f7af225
sciug <gaetano.sciurti@libero.it>
2021-10-11 14:24:06 +0200

    complete js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const distance = parseInt(prompt("quanti chilometri vuoi percorrere?"));
 const userAge = parseInt(prompt("quanti anni hai?"));
-const price = distance * 0.21
+const price = distance * 0.21;
+document.getElementById("age").textContent = userAge;
+document.getElementById("km").textContent = distance
 
 
  if (userAge < 18){
@@ -15,6 +17,6 @@ const price = distance * 0.21
      console.log("Final price equals: " + finalPrice.toFixed(2));
     
 }else{
-    var finalPrice = price - discount;
-     console.log("Final price equals: " + finalPrice.toFixed(2));
+    console.log(price)
+     document.getElementById("price").textContent = price.toFixed(2)
 }
